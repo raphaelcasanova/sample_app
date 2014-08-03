b023a2a4a0b5adf65422e55aae4fe964f11fedfc
Raphael Casanova <raphael.casanova@resisting-mind.com>
2014-08-03 22:27:02 +0200

    add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,14 +52,6 @@ describe "User Pages" do
     end
   end
 
-  describe 'profile page' do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
-
   describe "signup page" do
     before { visit signup_path }
 
@@ -159,6 +151,23 @@ describe "User Pages" do
         patch user_path(user), params
       end
       specify { expect(user.reload).not_to be_admin }
+    end
+
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }
+      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+      let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+      before { visit user_path(user) }
+
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+
+      describe "microposts" do
+        it { should have_content(m1.content) }
+        it { should have_content(m2.content) }
+        it { should have_content(user.microposts.count) }
+      end
     end
   end
 end
